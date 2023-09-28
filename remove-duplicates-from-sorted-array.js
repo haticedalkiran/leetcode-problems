82113ca2194797b19fa669d61095aa3c5518c32f
Hatice Dalkıran <kocakkhatice@gmail.com>
2023-09-28 12:30:13 +0300

    remove duplicates from array

---
+++ remove-duplicates-from-sorted-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var removeDuplicates = function (nums) {
+  let temp = nums[0];
+  if (nums.length === 1){
+    return nums.length;
+  }
+    
+  for (let i = 1; i < nums.length; i++) {
+    if (temp === nums[i]) {
+      nums.splice(i, 1);
+      i--;
+    } else {
+      temp = nums[i];
+    }
+  }
+  return nums.length;
+};
